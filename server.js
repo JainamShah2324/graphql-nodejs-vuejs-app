dc6436a3359579953a818c9ac83cfee99b0b0a97
Jainam Shah <jainamtirthshah1998@gmail.com>
2021-01-11 18:19:13 +0530

    Initial introduction to Types, Schema, List events and Create Events

--- server.js
+++ server.js
@@ -4,16 +4,33 @@ const { buildSchema } = require('graphql');
 
 const app = express();
 
+const events = [];
+
 app.use(express.json());
 
 app.use('/graphql', graphqlHTTP({
     schema: buildSchema(`
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
         type rootQuery {
-            events: [String!]!
+            events: [Event!]!
         }
 
         type rootMutation {
-            createEvent(name: String) : String
+            createEvent(eventInput: EventInput): Event
         }
 
         schema {
@@ -23,12 +40,19 @@ app.use('/graphql', graphqlHTTP({
     `),
     rootValue: {
         events: () => {
-            return ["programming", "gaming", "cooking", "problem solving"];
+            return events;
         },
 
         createEvent: (args) => {
-            const eventName = args.name;
-            return eventName;
+            const event = {
+                _id: Math.random().toString(),
+                title: args.eventInput.title,
+                description: args.eventInput.description,
+                price: parseFloat(args.eventInput.price),
+                date: args.eventInput.date
+            };
+            events.push(event);
+            return event;
         }
     },
     graphiql: true
